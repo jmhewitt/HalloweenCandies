8433c5836fc7f4effd565f5ebf6ca3ec7d05284c
Josh Hewitt <joshua.hewitt@colostate.edu>
2018-10-10 17:51:29 -0600

    basic data load scripts

--- load_data.R
+++ load_data.R
@@ -1,3 +1,22 @@
-# basic loading of data
+# basic loading for datasets
 
-train = 
+#
+# training data
+#
+
+# download and load from internet
+train = read.csv('https://raw.githubusercontent.com/jmhewitt/HalloweenCandies/master/train.csv')
+
+# OR, load the data if you have it on your computer
+train = read.csv('train.csv')
+
+
+#
+# test data (predictors only, no cheating!)
+#
+
+# download and load from internet
+test.predictors = read.csv('https://raw.githubusercontent.com/jmhewitt/HalloweenCandies/master/test.csv')
+
+# OR, load the data if you have it on your computer
+test.predictors = read.csv('test.csv')
